a303aded7006389bc8af66e3dd779969c722f6b6
elponsis <111057@tecnicatura.frc.utn.edu.ar>
2021-06-26 21:19:01 -0300

    back-end deliveries

--- server/build/controllers/deliveriesController.js
+++ server/build/controllers/deliveriesController.js
@@ -11,6 +11,32 @@ class DeliveriesController {
         console.log(services);
         res.json(services);
     }
+    //GET ENTREGAS
+    async getDeliveries(req, res) {
+        const deliveries = await database_1.default.query("SELECT * FROM entregas");
+        console.log(deliveries);
+        res.json(deliveries);
+    }
+    //GET ENTREGAS BY DNI
+    async getDeliverybyDni(req, res) {
+        const { id } = req.params;
+        const entregas = await database_1.default.query("SELECT * FROM entregas WHERE id = ?", [id]);
+        if (entregas.length > 0) {
+            return res.json(entregas[0]);
+        }
+        res.status(404).json({ text: 'Entrega no encontrada' });
+    }
+    //POST ENTREGA
+    async createDelivery(req, res) {
+        await database_1.default.query("INSERT INTO entregas set?", [req.body]);
+        res.json({ message: "Creando entrega" });
+    }
+    //DELETE ENTREGA NO SE SI HACE FALTA PERO LE MANDO X LAS DUDAS
+    async delete(req, res) {
+        const { id } = req.params;
+        await database_1.default.query("DELETE FROM entregas WHERE id = ?", [id]);
+        res.json({ message: "Entrega eliminada" });
+    }
 }
 const deliveriesController = new DeliveriesController();
 exports.default = deliveriesController;
