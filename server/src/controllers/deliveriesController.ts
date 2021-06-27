285d302572849f13da25a0bfb39ec5adc6624a3e
elponsis <111057@tecnicatura.frc.utn.edu.ar>
2021-06-26 22:07:14 -0300

    endpoint roles

--- server/src/controllers/deliveriesController.ts
+++ server/src/controllers/deliveriesController.ts
@@ -3,17 +3,25 @@ import pool from "../database";
 
 // Controlador de Entregas
 class DeliveriesController {
+
+  //GET SERVICIOS
   public async getServices(req: Request, res: Response): Promise<void> {
+    const roles = await pool.query("SELECT * FROM roles");
+    res.json(roles);
+  }
+
+  //GET ROLES
+  public async getRoles(req: Request, res: Response): Promise<void> {
     const services = await pool.query("SELECT * FROM servicios");
-    console.log(services);
     res.json(services);
   }
+
+
 
   //GET ENTREGAS
 
   public async getDeliveries(req: Request, res: Response): Promise<void> {
     const deliveries = await pool.query("SELECT * FROM entregas");
-    console.log(deliveries);
     res.json(deliveries);
   }
 
