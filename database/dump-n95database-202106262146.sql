109d5f4467a68380dc038cdf1725a266be2aeab1
Lucas Gordillo <lucas.gordillo@id90travel.com>
2021-06-28 21:26:00 -0300

    fix auto increment

--- database/dump-n95database-202106262146.sql
+++ database/dump-n95database-202106262146.sql
@@ -4,16 +4,16 @@
 -- ------------------------------------------------------
 -- Server version	8.0.25
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!50503 SET NAMES utf8mb4 */;
+-- /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+-- /*!40103 SET TIME_ZONE='+00:00' */;
+-- /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+-- /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+-- /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 CREATE DATABASE n95database;
 
@@ -22,8 +22,8 @@ CREATE DATABASE n95database;
 --
 
 DROP TABLE IF EXISTS `entregas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `entregas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
@@ -33,15 +33,15 @@ CREATE TABLE `entregas` (
   `idServicio` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `entregas`
 --
 
 LOCK TABLES `entregas` WRITE;
-/*!40000 ALTER TABLE `entregas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `entregas` ENABLE KEYS */;
+-- /*!40000 ALTER TABLE `entregas` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `entregas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -49,8 +49,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `personas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `personas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(65) DEFAULT NULL,
@@ -59,15 +59,15 @@ CREATE TABLE `personas` (
   `idRol` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `personas`
 --
 
 LOCK TABLES `personas` WRITE;
-/*!40000 ALTER TABLE `personas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `personas` ENABLE KEYS */;
+-- /*!40000 ALTER TABLE `personas` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `personas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -75,23 +75,23 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `roles`
 --
 
 LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` VALUES (1,'Dr/Dra'),(2,'Tecnico/a'),(3,'Auxiliar'),(4,'Enfermero/a'),(5,'Camillero/a'),(6,'Administrativo/a'),(7,'Licenciado/a'),(8,'Otro');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+-- /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -99,23 +99,23 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `servicios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `servicios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `servicios`
 --
 
 LOCK TABLES `servicios` WRITE;
-/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
 INSERT INTO `servicios` VALUES (1,'VARIOS'),(2,'ALERGIA'),(3,'ANESTESIA'),(4,'CARDIOLOGIA'),(5,'CIRUGIA GENERAL'),(6,'CIRUGIA'),(7,'CLINICA MEDICA'),(8,'CLINICA QUIRURGICA'),(9,'CONSULTORIOS EXTERNOS'),(10,'DERMATOLOGIA'),(11,'DIABETES'),(12,'DIAG POR IMAGEN'),(13,'DIALISIS'),(14,'ELECTROMEDICINA'),(15,'GUARDIA PEDIATRIA'),(16,'GASTROENTEROLOGIA'),(17,'GINECO-OBSTETRICIA'),(18,'GUARDIA CENTRAL'),(19,'GUARDIA DE OBSTETRICIA'),(20,'HEMOTERAPIA'),(21,'INFECTOLOGIA'),(22,'KINESIOLOGIA'),(23,'LABORATORIO'),(24,'LACTARIO'),(25,'LAVADERO'),(26,'LIMPIEZA'),(27,'MEDICINA FAMILIAR'),(28,'NEONATOLOGIA'),(29,'NEUROCIRUGIA'),(30,'NUTRICION'),(31,'OBSTETRICIA'),(32,'ODONTOLOGIA'),(33,'OFTALMOLOGIA'),(34,'QUIROFANO CENTRAL'),(35,'REHABILITACION'),(36,'UCI COVID'),(37,'UCI COVID 2'),(38,'UTI ADULTOS'),(39,'UTI COVID'),(40,'UTI COVID 2'),(41,'UTI NEO'),(42,'VACUNATORIO');
-/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+-- /*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -123,36 +123,36 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(55) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuarios`
 --
 
 LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+-- /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+-- /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'n95database'
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+-- /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+-- /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+-- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2021-06-26 21:46:15
