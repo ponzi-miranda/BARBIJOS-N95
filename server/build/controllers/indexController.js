f9c27e1737d714ec953f56f08df909b055935194
elponsis <111057@tecnicatura.frc.utn.edu.ar>
2021-07-04 03:55:03 -0300

    .

--- server/build/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexController = void 0;
-class IndexController {
-    index(req, res) {
-        res.json({ text: "API is in /api/XXXX" });
-    }
-}
-exports.indexController = new IndexController();
